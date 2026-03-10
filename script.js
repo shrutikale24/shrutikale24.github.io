98f6866ec9541a43f187d93e4452b1108e8c90d7
shrutikale24 <shrutikale930@gmail.com>
2026-03-10 22:02:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,22 @@
-/* Slideshow */
+function validateForm()
+{
 
-let slides=document.querySelectorAll(".slide");
+var name = document.getElementById("name").value;
+var email = document.getElementById("email").value;
 
-let index=0;
-
-function showSlides(){
-
-slides.forEach((slide)=>{
-slide.classList.remove("active");
-});
-
-index++;
-
-if(index>slides.length){
-index=1;
-}
-
-slides[index-1].classList.add("active");
-
-setTimeout(showSlides,3000);
-
-}
-
-showSlides();
-
-
-
-/* Form Validation */
-
-function validateForm(){
-
-let name=document.getElementById("name").value.trim();
-let email=document.getElementById("email").value.trim();
-
-if(name===""){
+if(name == "")
+{
 alert("Please enter your name");
 return false;
 }
 
-if(email===""){
+if(email == "")
+{
 alert("Please enter your email");
 return false;
 }
 
-alert("Thank you for contacting Solapur Tourism!");
-
+alert("Thank you! Your enquiry has been submitted.");
 return true;
 
 }
